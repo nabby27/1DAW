2b8a45595428ed88be83f0d77de41ec5b4a3f6de
nabby27 <ivancordoba77@gmail.com>
2018-10-01 18:42:59 +0200

    201810011842

--- Programación/Tema 2 - Elementos del lenguaje/Projects/ej1/src/catorce.java
+++ Programación/Tema 2 - Elementos del lenguaje/Projects/ej1/src/catorce.java
@@ -11,7 +11,25 @@
 public class catorce {
     
     public static void main(String[] args) {
+        
+        ej1();
+        System.err.println("");
+        System.err.println("");
+        ej2();
+        System.err.println("");
+        System.err.println("");
+        ej3();
+        System.err.println("");
+        System.err.println("");
+        ej4();
+        
+        
+    }
+    
+    public static void ej1() {
+        System.err.println("TABLA 1");
         int a, b, c;
+        System.err.println("------------------------------------");
         System.err.println("\t\tA \tB \tC");
         System.err.println("------------------------------------");
         a = 3;
@@ -26,6 +44,54 @@ public class catorce {
         System.err.println("b=c-a++; \t" + a + "\t" + b + "\t" + c);
         a = b * c;
         System.err.println("a=b*c; \t\t" + a + "\t" + b + "\t" + c);
-        
+        System.err.println("------------------------------------");
+    }
+    
+    public static void ej2() {
+        System.err.println("TABLA 2");
+        int c;
+        float x;
+        System.err.println("------------------------------------");
+        System.err.println("\t\tC \tX");
+        System.err.println("------------------------------------");
+        c = 6;
+        System.err.println("c=6; \t\t" + c);
+        c /= 4;
+        System.err.println("c/=4; \t\t" + c);
+        x = ++c;
+        System.err.println("x=++C; \t\t" + c + "\t" + x);
+        System.err.println("------------------------------------");
+    }
+    
+    public static void ej3() {
+        System.err.println("TABLA 3");
+        int c;
+        float x;
+        System.err.println("------------------------------------");
+        System.err.println("\t\tC \tX");
+        System.err.println("------------------------------------");
+        c = 6;
+        System.err.println("c=6; \t\t" + c);
+        c /= 4;
+        System.err.println("c/=4; \t\t" + c);
+        x = c++;
+        System.err.println("x=++C; \t\t" + c + "\t" + x);
+        System.err.println("------------------------------------");
+    }
+    
+    public static void ej4() {
+        System.err.println("TABLA 4");
+        int c;
+        float x;
+        System.err.println("------------------------------------");
+        System.err.println("\t\tC \tX");
+        System.err.println("------------------------------------");
+        x = 6;
+        System.err.println("c=6; \t\t\t" + x);
+        x /= 4;
+        System.err.println("c/=4; \t\t\t" + x);
+        c = (int) x++;
+        System.err.println("x=++C; \t\t" + c + "\t" + x);
+        System.err.println("------------------------------------");
     }
 }
