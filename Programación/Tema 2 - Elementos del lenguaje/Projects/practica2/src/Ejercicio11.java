74b7ad572d17393703bd13fc46410d7e487040a0
nabby27 <ivancordoba77@gamil.com>
2018-10-08 12:11:27 +0200

    201810081211

---
+++ Programación/Tema 2 - Elementos del lenguaje/Projects/practica2/src/Ejercicio11.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Iván Córdoba Donet
+ */
+public class Ejercicio11 {
+    
+    public static void main(String[] args) {
+        int alto = 15;
+        int ancho = 25;
+        int perimetro = 0;
+        int area = 0;
+        
+        perimetro = (((alto^2) + ((ancho/2)^2))*2);
+        area = ((alto * ancho) / 2);
+        
+        System.out.println("El perímetro del triangulo es: " + perimetro + " cm");
+        System.out.println("El area del triangulo es: " + area + " cm2");
+   }
+    
+}
---
+++ Programación/Tema 2 - Elementos del lenguaje/Projects/practica2/src/Ejercicio12.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Iván Córdoba Donet
+ */
+public class Ejercicio12 {
+    
+    static final float pi = 3.1415F;
+    
+    public static void main(String[] args) {
+        float diametro = 15.5F;
+        float altura = 42.4F;
+        float area = 0;
+        
+   }
+    
+}
