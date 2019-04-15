71565b75c8737e6c641bb5facaadfd55ac851848
nabby27 <ivancordoba77@gamil.com>
2019-04-15 12:08:59 +0200

    201904151208

--- Programación/Tema 9 - Acceso a Base de Datos/Projects/ConexionDB/src/ClientesDB/Main.java
+++ Programación/Tema 9 - Acceso a Base de Datos/Projects/ConexionDB/src/ClientesDB/Main.java
@@ -8,6 +8,8 @@ package ClientesDB;
 import DAO.DBConnection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 /**
  *
@@ -16,10 +18,28 @@ import java.sql.PreparedStatement;
 public class Main {
     
     public static void main(String[] args) {
+        Integer rowsUpdated = 0;
+        ResultSet rs = null;
         try {
             Connection con = DBConnection.openConnection();
             
-            Integer rowsUpdated = update(con);
+            rowsUpdated = update(con);
+            System.out.println(rowsUpdated + " rows updated");
+            
+            rowsUpdated = staticUpdate(con);
+            System.out.println(rowsUpdated + " rows updated");
+            
+            rs = select(con);
+            while (rs.next()) {
+                System.out.println(rs.getString("IdCategoria") + "\t" + rs.getString("NomCategoria") + "\t\t" + rs.getString("Descripcion"));
+            }
+            rs.close();
+            
+            insert(con);
+            staticInsert(con);
+            
+            delete(con);
+            staticDelete(con);
             
             DBConnection.closeConnection(con);
         } catch (Exception e) {
@@ -37,6 +57,100 @@ public class Main {
             pstmt.setString(1, newName);
             pstmt.setInt(2, idCategory);
             return pstmt.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+    
+    private static Integer staticUpdate(Connection con) {
+        try {
+            System.out.println("UPDATE a la tabla categorias");
+            String query = "UPDATE categorias SET NomCategoria = 'Bebidas' WHERE IdCategoria = 1";
+            Statement stmt = con.createStatement();
+            return stmt.executeUpdate(query);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+    
+    private static ResultSet select(Connection con) {
+        Integer idCategory = 1;
+        try {
+            System.out.println("SELECT categorias");
+            String query = "SELECT * FROM categorias WHERE IdCategoria = ?";
+            PreparedStatement pstmt = con.prepareStatement(query);
+            pstmt.setInt(1, idCategory);
+            return pstmt.executeQuery();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+    
+    private static ResultSet staticSelect(Connection con) {
+        try {
+            System.out.println("SELECT categorias");
+            String query = "SELECT * FROM categorias WHERE IdCategoria = 1";
+            Statement stmt = con.createStatement();
+            return stmt.executeQuery(query);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+    
+    private static Boolean insert(Connection con) {
+        Integer idCateogry = 10;
+        String nomCategory = "Otros";
+        String description = "Categoria sin identificar";
+        try {
+            System.out.println("INSERT a la tabla categorias");
+            String query = "INSERT INTO categorias (IdCategoria, NomCategoria, Descripcion) values(?, ?, ?)";
+            PreparedStatement pstmt = con.prepareStatement(query);
+            pstmt.setInt(1, idCateogry);
+            pstmt.setString(2, nomCategory);
+            pstmt.setString(3, description);
+            return pstmt.execute();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+    
+    private static Boolean staticInsert(Connection con) {
+        try {
+            System.out.println("INSERT a la tabla categorias");
+            String query = "INSERT INTO categorias (IdCategoria, NomCategoria, Descripcion) values(11, 'Ferreteria', 'productos de ferreteria')";
+            Statement stmt = con.createStatement();
+            return stmt.execute(query);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+    
+    private static Boolean delete(Connection con) {
+        Integer idCategory = 10;
+        try {
+            System.out.println("DELETE a la tabla categorias");
+            String query = "DELETE FROM categorias WHERE IdCategoria = ?";
+            PreparedStatement pstmt = con.prepareStatement(query);
+            pstmt.setInt(1, idCategory);
+            return pstmt.execute();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+    
+    private static Boolean staticDelete(Connection con) {
+        try {
+            System.out.println("DELETE a la tabla categorias");
+            String query = "DELETE FROM categorias WHERE IdCategoria = 11";
+            Statement stmt = con.createStatement();
+            return stmt.execute(query);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return null;
